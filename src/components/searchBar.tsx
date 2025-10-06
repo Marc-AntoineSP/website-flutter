433c96f5fc630c7be1b7252759f4423a40beb573
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-10-06 13:30:39 +0200

    ADD : searchbar

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,7 +1,21 @@
 export default function SearchBar(props:{message:string}){
     return (
-        <nav>
-            <div id="burger" className="btn btn-neutral">{props.message}</div>
+        <nav className="
+            flex gap-2.5 p-2.5 bg-neutral justify-center items-center
+        ">
+            <button>
+                <svg
+                    className="swap-off fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 512 512">
+                    <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+                </svg>
+            </button>
+            <input type="text" name="searchBar" id="searchBar" className="
+                input-primary input-xl bg-white rounded-lg h-full
+            "/>
         </nav>
     )
 }
