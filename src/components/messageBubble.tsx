6615f367c287bbb180a1be72a2fd354165ce9699
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-10-06 16:10:07 +0200

    FIX: bubble

--- src/components/messageBubble.tsx
+++ src/components/messageBubble.tsx
@@ -6,8 +6,15 @@ interface MessageProp {
 
 export default function MessageBubble(props:MessageProp){
     return (
-        <div className={`chat ${props.is_user1 ? "chat-start" : "chat-end"}`}>
+        <div className={`chat ${props.is_user1 ? "chat-start" : "chat-end"} flex gap-6 items-center`}>
+            {props.is_user1 ? (
+            <>
+            <img src="/assets/pic.png" className="rounded-full" width={50}/>
+            <p className="chat-bubble bg-accent">{props.message}</p></>
+            ) : <>
             <p className="chat-bubble bg-accent">{props.message}</p>
+            <img src="/assets/pic.png" className="rounded-full" width={50}/>
+            </>}
         </div>
     );
 }
